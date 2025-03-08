ca6758c224a7dd722f649933905d607fc4a37f55
Isaladwy <isaladwy@gmail.com>
2025-03-09 01:13:39 +0200

    creating header file

---
+++ app/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+    <img src='logo.png' alt='logo'/>
+    <h1>Welcome to this NextJS Course!</h1>
+    </>
+  )
+}
