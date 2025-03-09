47410f2ef2980d092bd6cd6c12789425de4839db
Isaladwy <isaladwy@gmail.com>
2025-03-09 17:50:09 +0200

    creating & importing css for the main header

--- components/main-header.jsx
+++ components/main-header.jsx
@@ -1,15 +1,17 @@
 import Link from 'next/link';
 import React from 'react';
 import logoImg from '@/assets/logo.png';
+import styles from './main-header.module.css';
+import Image from 'next/image';
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
+    <header className={styles.header}>
+      <Link className={styles.logo} href="/">
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevel Food
       </Link>
-      <nav>
+      <nav className={styles.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
