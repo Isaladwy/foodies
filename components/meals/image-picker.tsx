f09527c4509769a1799e66f1606a89a084ac5b49
Isaladwy <isaladwy@gmail.com>
2025-03-15 16:37:38 +0200

    Add image selection functionality to ImagePicker component

--- components/meals/image-picker.tsx
+++ components/meals/image-picker.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useRef } from 'react';
 import classes from './image-picker.module.css';
 
 export default function ImagePicker({
@@ -8,16 +10,30 @@ export default function ImagePicker({
   label: string;
   name: string;
 }) {
+  const imageInput = useRef<HTMLInputElement | null>(null);
+
+  function handlePickImage() {
+    imageInput.current?.click();
+  }
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
         <input
+          className={classes.input}
           type="file"
           id={name}
           accept="image/png, image/jpeg"
           name={name}
+          ref={imageInput}
         />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickImage}
+        >
+          Pick an Image
+        </button>
       </div>
     </div>
   );
